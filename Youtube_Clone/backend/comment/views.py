4aa759a4a6decf33c224bb3a46b2867c8ce6652e
rossebell <Rosy24dr@outlook.com>
2022-04-29 18:02:37 -0400

    created the comments_details function in the views file for the comment app.

--- Youtube_Clone/backend/comment/views.py
+++ Youtube_Clone/backend/comment/views.py
@@ -1,9 +1,12 @@
+from modulefinder import IMPORT_NAME
+from webbrowser import get
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from .models import Comment
 from .serializers import CommentSerializer
+from django.shortcuts import get_object_or_404
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
@@ -12,3 +15,11 @@ def get_all_comments(request):
         comments = Comment.objects.all()
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data, status = status.HTTP_200_OK)
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def comments_details(request, pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    if request.method == "GET":
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data, status= status.HTTP_200_OK)
