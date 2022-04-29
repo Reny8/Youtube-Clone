4a60191e6980e3d4b44a01c18a63070ae5b28ae7
Reny Reyes <renyreyes8@hotmail.com>
2022-04-29 17:47:10 -0400

    Created the functionality to get all the comments

--- Youtube_Clone/backend/drf_jwt_backend/urls.py
+++ Youtube_Clone/backend/drf_jwt_backend/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from comment.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
     path('api/cars/', include('cars.urls')),
+    path('api/comments/', include('comment.urls'))
 ]
