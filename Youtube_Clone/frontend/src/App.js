e9e029fde8790f145c3165a06368e7ea7f4991b6
Reny Reyes <renyreyes8@hotmail.com>
2022-05-03 19:22:42 -0400

    Created a VideoPage to display the title and description

--- Youtube_Clone/frontend/src/App.js
+++ Youtube_Clone/frontend/src/App.js
@@ -9,18 +9,21 @@ import RegisterPage from "./pages/RegisterPage/RegisterPage";
 // import VideosPage from "./pages/VideosPage/VideoPage";
 import Navbar from "./components/NavBar/NavBar";
 import HomePage from "./pages/HomePage/HomePage";
+import VideoPage from "./pages/VideosPage/VideoPage";
 // Component Imports
 import Footer from "./components/Footer/Footer";
 
 // Util Imports
 import PrivateRoute from "./utils/PrivateRoute";
+
 function App() {
 
   return (
-    <div class='page'>
+    <div className='page'>
       <Navbar/>
       <Routes>
-        <Route path="/" element={<PrivateRoute><HomePage /></PrivateRoute>} />
+        <Route path='/' element={<VideoPage />}/>
+        <Route path="/cars" element={<PrivateRoute><HomePage/></PrivateRoute>} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
--- Youtube_Clone/frontend/src/pages/VideosPage/VideoPage.js
+++ Youtube_Clone/frontend/src/pages/VideosPage/VideoPage.js
@@ -1,37 +1,48 @@
-import React, {useState,useEffect} from 'react';
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import useAuth from "../../hooks/useAuth";
 
 const VideoPage = (props) => {
-    const [videos, setVideos] = useState([]);
-    useEffect(() => {
-      fetchVideos();
-    }, []);
-  
-    async function fetchVideos() {
-      let response = await axios.get(
-        "https://www.googleapis.com/youtube/v3/search?q=cats&key=AIzaSyBoNpzKq9ePvOhN8XLIrvCsqcUZt1sSj5c&part=snippet&maxLength=5"
-      );
-      setVideos(response.data);
-      console.log(videos);
-    }
+  const [user, token] = useAuth();
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        let response = await axios.get(
+          "https://www.googleapis.com/youtube/v3/search?q=cats&key=AIzaSyBoNpzKq9ePvOhN8XLIrvCsqcUZt1sSj5c&part=snippet&maxLength=5",
+          {
+            headers: {
+              Authorization: "Bearer " + token,
+            },
+          }
+        );
+        setVideos(response.data);
+        console.log(videos);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchVideos();
+  }, [token]);
 
+  return (
+    <div>
+      {videos &&
+        videos.map((video) => {
+          <div>
+            <div>
+              <img scr={video.snippet.thumbnails.default.url} />
+            </div>
+            <div>
+              <h2>{video.snippet.title}</h2>
+            </div>
+            <div>
+              <p>{video.snippt.description}</p>
+            </div>
+          </div>;
+        })}
+    </div>
+  );
+};
 
-
-
-    return ( 
-        <div>
-            {videos && videos.map((video)=>{
-                <ul>
-                    <li>
-                        <img scr={video.snippet.thumbnails.default}/>
-                    </li>
-                    <li><h2>{video.snippet.title}</h2></li>
-                    <li><p>{video.snippt.description}</p></li>
-                    <li></li>
-                </ul>
-            })}
-        </div>
-     );
-}
- 
 export default VideoPage;
