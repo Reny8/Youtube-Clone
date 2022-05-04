9099bc87343167b6041bfbe079074fdd098dc427
Reny Reyes <renyreyes8@hotmail.com>
2022-05-04 10:37:24 -0400

    Added a key to the displayvideos

--- Youtube_Clone/frontend/src/components/DisplayVideos/DisplayVideos.jsx
+++ Youtube_Clone/frontend/src/components/DisplayVideos/DisplayVideos.jsx
@@ -3,24 +3,26 @@ import { Link } from "react-router-dom";
 
 const DisplayVideos = (props) => {
   return (
-      <div>
-        {props.videos.map((video) => {
-          <div>
+    <div>
+      {props.videos.map((video,index) => {
+        return (
+          <div key={index}>
             <div>
               <img
-                scr={video.snippet.thumbnails.default.url}
+                scr={video.snippet.thumbnails.default}
                 alt="No Image Found"
               />
             </div>
             <div>
               <h2>{video.snippet.title}</h2>
             </div>
-            {/* <div>
+            <div>
               <p>{video.snippet.description}</p>
-            </div> */}
-          </div>;
-        })}
-      </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
